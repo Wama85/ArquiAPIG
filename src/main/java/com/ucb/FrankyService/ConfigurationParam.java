67bbf27d78e027e9a0c1fd4415386ba000b7d64e
Wama85 <wi_an9@hotmail.com>
2024-09-14 10:58:10 -0400

    cambios en Param

--- src/main/java/com/ucb/FrankyService/ConfigurationParam.java
+++ src/main/java/com/ucb/FrankyService/ConfigurationParam.java
@@ -1,8 +1,8 @@
 package com.ucb.FrankyService;
 
-import org.springframework.beans.factory.annotation.Value;
+
 import org.springframework.context.MessageSource;
-import org.springframework.context.MessageSourceAware;
+
 import org.springframework.stereotype.Component;
 
 import java.util.Locale;
--- src/main/java/com/ucb/FrankyService/GreetingService.java
+++ src/main/java/com/ucb/FrankyService/GreetingService.java
@@ -1,6 +1,5 @@
 package com.ucb.FrankyService;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
